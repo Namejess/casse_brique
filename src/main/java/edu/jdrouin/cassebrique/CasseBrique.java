b9d83bb1e1e645fa2a922817c3ee5674c80e76fa
Namejess <jessy_drouin@protonmail.com>
2022-09-23 16:30:10 +0200

    Official commit

--- src/main/java/edu/jdrouin/cassebrique/CasseBrique.java
+++ src/main/java/edu/jdrouin/cassebrique/CasseBrique.java
@@ -1,11 +1,50 @@
 package edu.jdrouin.cassebrique;
 
-public class CasseBrique {
+import java.awt.*;
+import javax.swing.*;
 
+public class CasseBrique extends Canvas {
 
+    public CasseBrique() throws InterruptedException {
+        JFrame fenetre = new JFrame("Casse brique");
+        //On récupère le panneau de la fenetre principale
+        JPanel panneau = (JPanel) fenetre.getContentPane();
+        //On définie la hauteur / largeur de l'écran
+        panneau.setPreferredSize(new Dimension(500, 500));
+        setBounds(0, 0, 500,500);
+        //On ajoute cette classe (qui hérite de Canvas) comme composant du panneau principal
+        panneau.add(this);
 
-    public static void main(String[] args) {
-        new CasseBrique();
+        fenetre.pack();
+        fenetre.setResizable(false);
+        fenetre.setLocationRelativeTo(null);
+        fenetre.setVisible(true);
+        fenetre.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        fenetre.requestFocus();
+
+        //On indique que le raffraichissement de l'ecran doit être fait manuellement.
+        createBufferStrategy(2);
+        setIgnoreRepaint(true);
+        this.setFocusable(false);
+
+        demarrer();
     }
 
+    public void demarrer() throws InterruptedException {
+
+        long indexFrame = 0;
+
+        while(true) {
+            indexFrame ++;
+
+            System.out.println(indexFrame);
+
+
+            Thread.sleep(1000 / 60);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new CasseBrique();
+    }
 }
